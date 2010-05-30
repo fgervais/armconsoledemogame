d89dba3b614161ce3235f41d690bad80ddc4b110
fournierseb2 <fournierseb2@051cbfc0-75b8-dce1-6088-688cefeb9347>
2010-05-30 05:08:43 +0000

    Continuation du buster...

---
+++ trunk/src/game/megaman_demo/states/MegamanRunningLeftShot.h
@@ -0,0 +1,31 @@
+/*
+ * MegamanRunningLeftShot.h
+ *
+ *  Created on: 2010-05-29
+ *      Author: Basse
+ */
+
+#ifndef MEGAMANRUNNINGLEFTSHOT_H_
+#define MEGAMANRUNNINGLEFTSHOT_H_
+
+#include "MegamanState.h"
+
+class MegamanRunningLeftShot: public MegamanState {
+public:
+	static MegamanState* getInstance();
+
+	// Base class function override
+	virtual void jump(Megaman*);
+	virtual void runRight(Megaman*);
+	virtual void stopRunning(Megaman*);
+	virtual void initialize(Megaman*);
+	virtual void update(Megaman*);
+
+private:
+	MegamanRunningLeftShot(uint32_t animationWidth, uint32_t animationHeight, Bitmap** animationFrames, uint32_t numberOfFrame, Bitmap** animationMasks);
+	virtual ~MegamanRunningLeftShot();
+
+	static MegamanState* instance;
+};
+
+#endif /* MEGAMANRUNNINGLEFTSHOT_H_ */
