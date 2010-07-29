31964e554ec73279bd7af1f981c2a24dca0d38d1
ber.emile <ber.emile@051cbfc0-75b8-dce1-6088-688cefeb9347>
2010-07-29 07:29:17 +0000

    AJOUT: classe String imitant les String habituelle, c'est une classe contenant un char* et un uint32_t length.
    Elle permet aussi de concaténer 2 Strings:
    ex: String test("something");  test.append("something else");
    
    La longeur s'ajuste lors de concaténation. Ensuite quelque fonction de concaténation de char* y sont intégré pour plus de confort.

---
+++ src/game/engine/String.h
@@ -0,0 +1,33 @@
+/*
+ * String.h
+ *
+ *  Created on: 2010-07-29
+ *      Author: Emile
+ */
+
+#ifndef STRING_H_
+#define STRING_H_
+
+#include <stdio.h>
+
+class String {
+public:
+	String();										// default new string without any value
+	String(char *);									// new string with new value
+	virtual ~String();
+	char * value(){return this->content;}			// get String value
+	uint32_t lenght(){return this->lengthvalue;}	// get String length
+	void append(const char *);						// append a char array to the current String
+	void append(String);							// append another string to the current one
+
+
+	static uint32_t getLength(const char*);
+	static char * concatString(const char*, const char*);
+	static char * concatString(const char*, const uint32_t, const char*, const uint32_t);
+private:
+	char * content;
+	uint32_t lengthvalue;
+
+};
+
+#endif /* STRING_H_ */
